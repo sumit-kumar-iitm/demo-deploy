6a20db80f8de058481a98aff533604ae914d041b
Sumit Kumar <sumit.kumar1310@gmail.com>
2018-08-25 09:39:33 +0530

    initial commit

---
+++ src/main/java/com/observeai/example/demodeploy/DemodeployApplication.java
@@ -0,0 +1,12 @@
+package com.observeai.example.demodeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodeployApplication.class, args);
+	}
+}
---
+++ src/test/java/com/observeai/example/demodeploy/DemodeployApplicationTests.java
@@ -0,0 +1,16 @@
+package com.observeai.example.demodeploy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemodeployApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
