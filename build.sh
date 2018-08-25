db961795b0b63a205b7f6b926b2e2ed0fa71a232
Sumit Kumar <sumit.kumar1310@gmail.com>
2018-08-25 22:20:30 +0530

    tagging

--- build.sh
+++ build.sh
@@ -56,9 +56,11 @@ if [[ $GIT_BRANCH == */release* ]]; then
 	fi
 
 	export FINAL_VERSION=$RELEASE_VERSION.$BUILD_NUMBER
+    git tag $FINAL_VERSION
+    git push origin release
 	echo "Building version $FINAL_VERSION"
 
-   find . -type f -name "pom.xml" -exec sed -i -e "s/0\.0\.1-SNAPSHOT/${FINAL_VERSION}/g" {} +
+    find . -type f -name "pom.xml" -exec sed -i -e "s/0\.0\.1-SNAPSHOT/${FINAL_VERSION}/g" {} +
 
    #localPath=`pwd`
    #$localPath/checkmarx.sh
