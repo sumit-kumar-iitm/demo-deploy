189af7ea8f2e331a77ad2bd04b85b6b543a92db0
sumit-kumar-iitm <sumit.kumar1310@gmail.com>
2018-08-27 20:27:00 +0530

    dummy commit

--- build.sh
+++ build.sh
@@ -16,7 +16,9 @@ echo "GIT BRANCH $GIT_BRANCH"
 
 # Define params based on whether this is a snapshot or Release build
 # Make it case insensitive (Release or release)
-echo "Running a Release build"
+if [[ $GIT_BRANCH == */release* ]]; then
+
+	echo "Running a Release build"
 
 #   	export VERSION_SRC_DIR=`dirname $0`
 #   	. $VERSION_SRC_DIR/build.properties
@@ -44,37 +46,4 @@ echo "Running a Release build"
 
    # build and publish/install to nexus app artifact (CI Build)
    $MAVEN_COMMAND $MVN_TARGETS #$MVNPARAMS
-
-
-
-#if [[ $GIT_BRANCH == */release* ]]; then
-#
-#	echo "Running a Release build"
-#
-##   	export VERSION_SRC_DIR=`dirname $0`
-##   	. $VERSION_SRC_DIR/build.properties
-##   	if [ -z $RELEASE_VERSION  ]; then
-##		echo "Make sure build.properties is present in the root directory\
-## 		of the git repo and contains value for variable RELEASE_VERSION"
-##		exit 1
-##	else
-##		num_digits=$(echo $RELEASE_VERSION | tr '.' '\n' | wc -l)
-##		if [ $num_digits != 3 ]; then
-##			echo "Give only three digits for variable RELEASE_VERSION, example 1.2.3"
-##			exit 1
-##		fi
-##	fi
-##
-##	export FINAL_VERSION=$RELEASE_VERSION.$BUILD_NUMBER
-##	echo "Building version $FINAL_VERSION"
-#
-#    #find . -type f -name "pom.xml" -exec sed -i -e "s/0\.0\.1-SNAPSHOT/${FINAL_VERSION}/g" {} +
-#
-#   export MVN_TARGETS="release:clean release:prepare release:perform -Djavax.xml.accessExternalSchema=all"
-#
-#   # All maven commands in this scrip must use the -s option
-#   export MAVEN_COMMAND="./mvnw -B -s settings.xml"
-#
-#   # build and publish/install to nexus app artifact (CI Build)
-#   $MAVEN_COMMAND $MVN_TARGETS #$MVNPARAMS
-#fi
+fi
